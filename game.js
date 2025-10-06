e2c4706c9c5e91436e570abb76f8a7c096e16083
alaminibnefarukayon <alaminibnefarukayon@gmail.com>
2025-10-06 08:44:44 +0600

    version 0.0002

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ let randomNum = Math.floor(Math.random() * 10);
 console.log(randomNum);
 let Ans = Number(prompt("Enter a number"));
 while (Ans !== randomNum) {
+  console.log("You lost!! Try again Your number was " + Ans);
   Ans = Number(prompt("You Lost!! Try again; Enter a number"));
 }
 console.log("You win!!");
